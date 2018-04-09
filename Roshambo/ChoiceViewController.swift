38377d83b279ccf6a6fb363231702b5bfc33efff
omrobbie <putu.roby@yahoo.com>
2018-04-09 17:19:17 +0800

    beri nama view controller di storyboard

---
+++ Roshambo/ChoiceViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  Roshambo
+//
+//  Created by omrobbie on 09/04/18.
+//  Copyright © 2018 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ChoiceViewController: UIViewController {
+    
+}
---
+++ Roshambo/ResultViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ResultViewController.swift
+//  Roshambo
+//
+//  Created by omrobbie on 09/04/18.
+//  Copyright © 2018 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+}
