90673a09a3882c15e4628ea0e2ae089d2643e3d2
omrobbie <putu.roby@yahoo.com>
2018-04-10 09:12:42 +0800

    membuat method yang akan menampilkan hasil gambar

--- Roshambo/ChoiceViewController.swift
+++ Roshambo/ChoiceViewController.swift
@@ -11,11 +11,26 @@ import UIKit
 class ChoiceViewController: UIViewController {
     
     @IBAction private func playRock(_ sender: UIButton) {
-        let viewController = self.storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as! ResultViewController 
+        let viewController = self.storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as! ResultViewController
+        viewController.userChoice = getUserShape(sender)
         present(viewController, animated: true, completion: nil)
     }
     
     @IBAction private func playPaper(_ sender: UIButton) {
         performSegue(withIdentifier: "play", sender: sender)
     }
+    
+    private func getUserShape(_ sender: UIButton) -> Shape {
+        // Titles are set to one of: Rock, Paper, or Scissors
+        let shape = sender.title(for: UIControlState())!
+        return Shape(rawValue: shape)!
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "play" {
+            let viewController = segue.destination as! ResultViewController
+            viewController.userChoice = getUserShape(sender as! UIButton)
+        }
+    }
+
 }
--- Roshambo/ResultViewController.swift
+++ Roshambo/ResultViewController.swift
@@ -23,4 +23,42 @@ enum Shape: String {
 
 class ResultViewController: UIViewController {
     
+    @IBOutlet private weak var resultImage: UIImageView!
+    @IBOutlet private weak var resultLabel: UILabel!
+    
+    var userChoice: Shape!
+    private let opponentChoice: Shape = Shape.randomShape()
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        displayResult()
+    }
+    
+    private func displayResult() {
+        var imageName: String
+        var text: String
+        let matchup = "\(userChoice.rawValue) vs. \(opponentChoice.rawValue)"
+        
+        switch (userChoice!, opponentChoice) {
+        case let (user, opponent) where user == opponent:
+            text = "\(matchup): it's a tie!"
+            imageName = "tie"
+        case (.Rock, .Scissors), (.Paper, .Rock), (.Scissors, .Paper):
+            text = "You win with \(matchup)!"
+            imageName = "\(userChoice.rawValue)-\(opponentChoice.rawValue)"
+        default:
+            text = "You lose with \(matchup) :(."
+            imageName = "\(opponentChoice.rawValue)-\(userChoice.rawValue)"
+        }
+        
+        imageName = imageName.lowercased()
+        resultImage.image = UIImage(named: imageName)
+        resultLabel.text = text
+    }
+    
+    @IBAction private func playAgain() {
+        dismiss(animated: true, completion: nil)
+    }
+
 }
